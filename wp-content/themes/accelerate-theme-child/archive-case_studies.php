464133b56183435ba00a610bd5f8bf9ea95e7aee
kahn_s <kahn.shari2@gmail.com>
2020-11-10 11:03:58 +0200

    week 2 changes

--- wp-content/themes/accelerate-theme-child/archive-case_studies.php
+++ wp-content/themes/accelerate-theme-child/archive-case_studies.php
@@ -16,7 +16,7 @@ get_header(); ?>
             <?php while ( have_posts() ) : the_post(); 
                 $image_1 = get_field('image_1');
                 $size="full";
-                $services = get_field('services');?>
+                $services=get_field('services');?>
 
                 <article class="case-study">
 			        <aside class="case-study-sidebar">
